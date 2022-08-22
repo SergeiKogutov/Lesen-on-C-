2684b037e9647ca1958b9bd0c94672e9428dd90a
SergeiKogutov <SerVik2003@gmail.com>
2022-08-22 08:31:03 +0700

    test

---
+++ C#/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите номер дня нидели: ");
+int x = Convert.ToInt32( Console.ReadLine());
+
+if (x==1)
+{
+    Console.WriteLine(x+" это пнидельник ");
+}
+else if (x==2)
+{
+    Console.WriteLine(x+" это вторник ");
+}
+else if (x==3)
+{
+    Console.WriteLine(x+" это среда ");
+}
+else if (x==4)
+{
+    Console.WriteLine(x+" это четверг ");
+}
+else if (x==5)
+{
+    Console.WriteLine(x+" это пятница ");
+}
+else if (x==6)
+{
+    Console.WriteLine(x+" это суббота ");
+}
+else if (x==7)
+{
+    Console.WriteLine(x+" это воскрисенье ");
+}
+else
+{
+   Console.WriteLine(" такова дня недели нет  ");
+}
---
+++ new_program/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("введите два целых числа: ");
+// int x = Convert.ToInt32( Console.ReadLine());
+// int y = Convert.ToInt32( Console.ReadLine());
+
+// if (x*x==y)
+// {
+//      Console.WriteLine(x+" является корнем "+y);
+// }
+// else
+// {
+//     Console.WriteLine(x+" не является корнем "+y);
+// }
+
+// Console.WriteLine("введите целое число: ");
+// int x = Convert.ToInt32( Console.ReadLine());
+// int y = -x ;
+// while (y <= x)
+// {
+//     Console.WriteLine(y);
+//     y = y + 1;
+// }
+
+// Console.WriteLine("введите целое трёхзняное число: ");
+// string x = Console.ReadLine();
+// Console.WriteLine(x[2]);
+
+// Console.Write ("Введите целое трёхзначное число ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(x%10);
